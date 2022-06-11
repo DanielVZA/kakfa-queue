8c4d271ccb495763906b4100af516712d9a54fd6
DanielVZA <danielvasquez@protonmail.com>
2022-06-11 13:18:53 -0400

    First Commit

---
+++ src/KafkaServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kafka;
+
+use Illuminate\Support\ServiceProvider;
+
+class KafkaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $manager = $this->app['queue'];
+
+        $manager->addConnector('kafka', function () {
+            return new KafkaConnector;
+        });
+    }
+}
